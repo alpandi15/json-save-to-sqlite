f9f19af5e305aec49cb70ad33b9bd0a3bb83e51e
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2020-10-05 16:55:22 +0700

    Getting data with param ID

--- src/controlers/get/getController.js
+++ src/controlers/get/getController.js
@@ -5,32 +5,40 @@ import axios from 'axios'
 import { dataHadits } from '../../services/get/getService'
 
 export const getShadist = async (req, res) => {
-    const hadits = await dataHadits()
-    console.log('Total Hadits ', (hadits[0].totalHadits).split('-'))
-    let index = 0
-        let jsonData = []
-        let kitab
-        const numberHadits = (hadits[index].totalHadits).split('-')
-        let numberOfHadits = Number(numberHadits[0])
-        const totalHadits =  Number(numberHadits[1])
+    const { params } = req
 
-        corn.schedule('*/5 * * * * *', async () => {
-            kitab = hadits[index].name
-            if (numberOfHadits < 3) {
-                console.log('Total Hadits ', totalHadits)
-                const query = `http://api.carihadis.com/?kitab=${kitab}&id=${numberOfHadits}`
-
-                console.log('GET ', query)
-                const response = await axios.get(query)
-                jsonData.push(response.data.data['1'] || {})
-                numberOfHadits += 1
-
-            } else {
-                console.log('Selesai')
-                console.log(jsonData)
-                // fs.writeFileSync(`src/public/data_json/${kitab}.json`, JSON.stringify(jsonData))
-                index += 1,
-                numberOfHadits = 1
-            }
+    if (!params.id) {
+        res.send({
+            message: 'Required parameter ID of Hadits'
         })
+    }
+    const hadits = await dataHadits()
+    let index = params.id
+    console.log('Params: ', index)
+
+    let jsonData = []
+    let kitab
+    const numberHadits = (hadits[index].totalHadits).split('-')
+    let numberOfHadits = Number(numberHadits[0])
+    const totalHadits =  Number(numberHadits[1])
+
+    corn.schedule('*/5 * * * * *', async () => {
+        kitab = hadits[index].name
+        if (numberOfHadits < totalHadits) {
+            console.log('Total Hadits ', totalHadits)
+            const query = `http://api.carihadis.com/?kitab=${kitab}&id=${numberOfHadits}`
+
+            console.log('GET ', query)
+            const response = await axios.get(query)
+            jsonData.push(response.data.data['1'] || {})
+            numberOfHadits += 1
+
+        } else {
+            console.log('Selesai')
+            console.log(jsonData)
+            fs.writeFileSync(`src/public/data_json/${kitab}.json`, JSON.stringify(jsonData))
+            index += 1,
+            numberOfHadits = 1
+        }
+    })
 }
--- src/routes/v1/getRouter.js
+++ src/routes/v1/getRouter.js
@@ -3,6 +3,6 @@ import { getShadist } from '../../controlers/get/getController'
 
 const router = express.Router()
 
-router.get('/get-hadits', getShadist)
+router.get('/get-hadits/:id', getShadist)
 
 export default router
