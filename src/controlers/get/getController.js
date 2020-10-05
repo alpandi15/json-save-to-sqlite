eae6d32faa2ba1feeadcd9f45a67284b59baa2b9
Muhammad Al-Pandi <mh2.alpandi@yahoo.co.id>
2020-10-05 16:42:19 +0700

    Add Cron

--- src/controlers/get/getController.js
+++ src/controlers/get/getController.js
@@ -5,26 +5,32 @@ import axios from 'axios'
 import { dataHadits } from '../../services/get/getService'
 
 export const getShadist = async (req, res) => {
-    const kitab = 'Shahih_Bukhari'
-    let numberOfHadits = 1
-    const totalHadits = 7008
-
     const hadits = await dataHadits()
-    res.send(hadits)
-    // corn.schedule('*/5 * * * * *', async () => {
-    //     if (1 >= numberOfHadits) {
-    //         const query = `http://api.carihadis.com/?kitab=${kitab}&id=${numberOfHadits}`
-    //         const response = await axios.get(query)
-    //         const jsonString = JSON.stringify(response.data)
+    console.log('Total Hadits ', (hadits[0].totalHadits).split('-'))
+    let index = 0
+        let jsonData = []
+        let kitab
+        const numberHadits = (hadits[index].totalHadits).split('-')
+        let numberOfHadits = Number(numberHadits[0])
+        const totalHadits =  Number(numberHadits[1])
 
-    //         fs.writeFileSync(`src/public/data_json/Shahih_Bukhari.json`, jsonString)
-    //         console.log('query', response)
-    //     } else {
-    //         res.send({
-    //             message: 'Success'
-    //         })
-    //     }
+        corn.schedule('*/5 * * * * *', async () => {
+            kitab = hadits[index].name
+            if (numberOfHadits < 3) {
+                console.log('Total Hadits ', totalHadits)
+                const query = `http://api.carihadis.com/?kitab=${kitab}&id=${numberOfHadits}`
 
-    //     numberOfHadits += 1
-    // })
+                console.log('GET ', query)
+                const response = await axios.get(query)
+                jsonData.push(response.data.data['1'] || {})
+                numberOfHadits += 1
+
+            } else {
+                console.log('Selesai')
+                console.log(jsonData)
+                // fs.writeFileSync(`src/public/data_json/${kitab}.json`, JSON.stringify(jsonData))
+                index += 1,
+                numberOfHadits = 1
+            }
+        })
 }
